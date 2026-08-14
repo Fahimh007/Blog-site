c409eda292f44ff4499deb2dc45e481d3087f2ff
Fahimh007 <hasan35-1177@s.diu.edu.bd>
2026-08-15 00:51:42 +0600

    add blog & catagory model

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -121,6 +121,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'blog_main' / 'static',
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
