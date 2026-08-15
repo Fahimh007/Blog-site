ecdbbfb8b3a0e6023a5386b1787901086cadade5
Fahimh007 <hasan35-1177@s.diu.edu.bd>
2026-08-15 23:55:04 +0600

    blogs are added in db

--- blog_main/views.py
+++ blog_main/views.py
@@ -2,7 +2,16 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 # from ../templates import *
+from blogs.models import Category, Blog
 
 def home(request):
-    # return HttpResponse("<h1>Hello, welcome to my blog!</h1>")
-    return render(request, 'home.html')
+    categories = Category.objects.all()
+    featured_post = Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')
+    recent_post =  Blog.objects.filter(is_featured=False, status='Published').order_by('updated_at')
+    context = {
+        'categories': categories,
+        'featured_post': featured_post,
+        'recent_post': recent_post,
+    }
+    print(recent_post)
+    return render(request, 'home.html', context)
