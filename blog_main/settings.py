1e5f9e54dddb7fcf7816757bf156baf5029c4052
Fahimh007 <hasan35-1177@s.diu.edu.bd>
2026-08-23 22:04:09 +0600

    modified requirements.txt file

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -12,19 +12,22 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7vt_b6!@@$0d2d7p5gor1tdc*@roeahg8zomuovewvbp5#)ait'
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
